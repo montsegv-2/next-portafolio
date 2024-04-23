cf123ab4d2e97c877e523ca1309aa5a4817d8de4
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-04-23 16:22:55 -0600

    Add hamburger menu

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,12 @@
 import Link from "next/link";
 
+const links = [
+  { url: "/", title: "Home" },
+  { url: "/about", title: "About" },
+  { url: "/porfolio", title: "portfolio" },
+  { url: "/contact", title: "Contact" },
+];
+
 const Navbar = () => {
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
@@ -18,11 +25,19 @@ const Navbar = () => {
       {/* RESPONSIVE MENU */}
       <div className="">
         {/* MENU BUTTON */}
-        <button className="w-10 h-8 flex">
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
-          <div className="w-10 h-1 bg-black rounded"></div>
+        <button className="w-10 h-8 flex flex-col justify-between z-50 relative">
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
+          <div className="w-10 h-1 bg-white rounded"></div>
         </button>
+        {/* MENU LIST */}
+        <div className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl">
+          {links.map((link) => (
+            <Link href={link.url} key={link.title}>
+              {link.title}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
