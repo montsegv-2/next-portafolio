0174a38574ff998495969e53ebda4874299ca1f4
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-04-25 16:57:12 -0600

    Add button select in the navbar component

--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -6,10 +6,15 @@ import { usePathname } from "next/navigation";
 const NavLink = ({ link }) => {
   const pathName = usePathname();
 
-  console.log(pathName);
+  // console.log(pathName);
 
   return (
-    <Link className="" href={link.url}>
+    <Link
+      className={`rounded p-1 ${
+        pathName === link.url && "bg-black text-white"
+      } `}
+      href={link.url}
+    >
       {link.title}
     </Link>
   );
