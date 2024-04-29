a4669e50feaab1b1d617c0563501fc7e46fe3cf5
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-04-29 12:01:01 -0600

    Fix attribute of Image component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ const Homepage = () => {
     <div className="h-full flex flex-col">
       {/* IMAGE CONTAINER */}
       <div className="h-1/2 relative">
-        <Image src="/hero.png" alt="profile photo" fill />
+        <Image src="/hero.png" alt="" width={500} height={500} />
       </div>
       {/* TEXT CONTAINER */}
       <div className="h-1/2 flex flex-col gap-8 items-center justify-center">
