5621e8705ee759e0754084a3a9fdf25e44669fe0
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-04-29 11:59:27 -0600

    Fix sizes of responsive mode

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ import NavLink from "./navLink";
 const links = [
   { url: "/", title: "Home" },
   { url: "/about", title: "About" },
-  { url: "/porfolio", title: "portfolio" },
+  { url: "/portfolio", title: "Portfolio" },
   { url: "/contact", title: "Contact" },
 ];
 
@@ -16,7 +16,7 @@ const Navbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+    <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
       {/* LINKS */}
       <div className="hidden md:flex gap-4 w-1/3">
         {links.map((link) => (
@@ -24,7 +24,7 @@ const Navbar = () => {
         ))}
       </div>
       {/* LOGO */}
-      <div className="md:hidden lg:flex w-1/3 justify-center">
+      <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
         <Link
           href="/"
           className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center"
