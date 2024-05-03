ae64a53b7e5f645137d84225fef1e39bf759d7ff
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2024-05-03 15:31:47 -0600

    Fix background

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,19 +2,25 @@ import Image from "next/image";
 
 const Homepage = () => {
   return (
-    <div className="h-full flex flex-col">
+    <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
       {/* IMAGE CONTAINER */}
-      <div className="h-1/2 relative">
-        <Image src="/hero.png" alt="" width={500} height={500} />
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <Image
+          src="/hero.png"
+          alt="profile"
+          width={400}
+          height={400}
+          className="object-contain"
+        />
       </div>
       {/* TEXT CONTAINER */}
-      <div className="h-1/2 flex flex-col gap-8 items-center justify-center">
+      <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
         {/* TITLE */}
-        <h1 className="text-4xl font-bold">
+        <h1 className="text-4xl md:text-6xl font-bold">
           Frontend developer junior, from frontend to beyond.
         </h1>
         {/* DESCRIPTION */}
-        <p className="">
+        <p className="md:text-xl">
           Code my canvas, craft your vision. I'm not just a developer, I'm a
           Software Craftsman, Architect, and Full-Stack Developer, building the
           hidden magic that powers captivating experiences. Front-end finesse
@@ -22,11 +28,11 @@ const Homepage = () => {
           delight in mind.
         </p>
         {/* BUTTONS */}
-        <div className="">
-          <button className="p-4 rounded-lg ring-1 ring-back">
+        <div className="flex gap-4 pb-5">
+          <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
             View my work
           </button>
-          <button className="p-4 rounded-lg ring-1 ring-back">
+          <button className="p-4 rounded-lg ring-1 ring-black">
             Contact me
           </button>
         </div>
